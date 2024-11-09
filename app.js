d9fcbe2a0cc149b28dc6e896cefbaa441824ae45
stevesdiary <stephenoyeyemi@gmail.com>
2024-11-09 16:31:35 +0100

    Start server connect DB

--- app.js
+++ app.js
@@ -1,12 +1,17 @@
 // import PaystackPop from '@paystack/inline-js'
-const PaystackPop = require("@paystack/inline-js");
+// const PaystackPop = require("@paystack/inline-js");
 const express = require("express");
 const mongoose = require("mongoose");
 const app = express();
+// const { eventRoute } = require("./routes/eventRoute");
 app.use(express.json());
 
-app.use(express.urlencoded({ extended:}))
-// app.use('/'api/)
+
+
+app.use(express.urlencoded({ extended: true }));
+app.use('/api', (req, res) => {
+	return res.send({ message: 'App is running' });
+})
 
 
 module.exports = app;
