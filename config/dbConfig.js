676a000268334ebb69fd397a138be64348080499
steve-aella <stephenoyeyemi@gmail.com>
2024-11-12 16:01:09 +0100

    Use Relational database

--- config/dbConfig.js
+++ config/dbConfig.js
@@ -1,20 +1,60 @@
 require('dotenv').config();
-const { MongoClient } = require("mongodb");
-const MONGODB_URL = process.env.MONGODB_URL;
-const client = new MongoClient(MONGODB_URL);
+// const { MongoClient } = require("mongodb");
+// const MONGODB_URL = process.env.MONGODB_URL;
+// const client = new MongoClient(MONGODB_URL);
 
 
-const connection = async () => {
-  try {
-    await client.connect();
-    console.log("Database connected!");
-  } catch (error) {
-    console.error(error);
-    throw new Error("Connection Error", error);
-  }
-  finally {
-    client.close();
-  }
+// const connection = async () => {
+//   try {
+//     await client.connect();
+//     console.log("Database connected!");
+//   } catch (error) {
+//     console.error(error);
+//     throw new Error("Connection Error", error);
+//   }
+//   finally {
+//     client.close();
+//   }
+// };
+
+// connection().catch(console.error);
+
+const { Pool } = require('pg');
+const url = require('url');
+
+const config = {
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  database: process.env.DB_NAME,
+  ssl: {
+    rejectUnauthorized: true,
+    ca: process.env.CERTIFICATE,
+  },
 };
 
-connection().catch(console.error);
+const db = new Pool(config);
+
+async function queryDatabase() {
+  const client = await db.connect();
+  try {
+    const res = await client.query("SELECT VERSION()");
+    console.log("Connected to:", res.rows[0].version);
+  } catch (err) {
+    console.error('Query error: ', err);
+  } finally {
+    client.release();
+  }
+}
+
+queryDatabase();
+
+process.on('SIGTERM', () => {
+  pool.end(() => {
+    console.log('Pool has ended');
+    process.exit(0);
+  });
+});
+
+module.exports = { db };
