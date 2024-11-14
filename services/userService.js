8c3df716c1866ba1b07e846294bfa91c80420c4f
steve-aella <stephenoyeyemi@gmail.com>
2024-11-14 08:44:47 +0100

    Create userService.js

---
+++ services/userService.js
@@ -0,0 +1,21 @@
+const validator = require("../middlewares/validator");
+const { User } = require("../models");
+const userController = require('../controllers/userController');
+const userService = {
+	createUser: async(payload) => {
+		try {
+			const { error } = validator.User.validate(payload);
+			if (error) {
+					throw new Error(error.details[0].message);
+			}
+			const createUser = await User.create(payload);
+			// console.log("RESULT", createUser);
+			return createUser;
+		} catch (error) {
+			return error;
+		}
+
+	}
+}
+
+module.exports = { userService };
