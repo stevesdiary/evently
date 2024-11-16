c7ba7b7066e9930fad6d98d46dee565d69803fee
stevesdiary <stephenoyeyemi@gmail.com>
2024-11-16 19:22:15 +0100

    Create 20241112134952-create-rsvp.js
    create migration file for RSVP

---
+++ migrations/20241112134952-create-rsvp.js
@@ -0,0 +1,54 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('RSVPs', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      event_id: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      start_date: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+      end_date: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+      start_time: {
+        type: Sequelize.TIME,
+        allowNull: false,
+      },
+      end_time: {
+        type: Sequelize.TIME,
+      },
+      price: {
+        type: Sequelize.INTEGER,
+      },
+      venue: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      payment_status: {
+        type: Sequelize.STRING,
+      },
+      created_at: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updated_at: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('RSVPs');
+  }
+};
