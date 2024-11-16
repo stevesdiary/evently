355939831375a90a1fcf0de9a7dc0236dc51e979
stevesdiary <stephenoyeyemi@gmail.com>
2024-11-16 19:21:28 +0100

    Update dbConfig.js

--- config/dbConfig.js
+++ config/dbConfig.js
@@ -1,37 +1,16 @@
 require('dotenv').config();
-// const { MongoClient } = require("mongodb");
-// const MONGODB_URL = process.env.MONGODB_URL;
-// const client = new MongoClient(MONGODB_URL);
-
-
-// const connection = async () => {
-//   try {
-//     await client.connect();
-//     console.log("Database connected!");
-//   } catch (error) {
-//     console.error(error);
-//     throw new Error("Connection Error", error);
-//   }
-//   finally {
-//     client.close();
-//   }
-// };
-
-// connection().catch(console.error);
-
 const { Pool } = require('pg');
-const url = require('url');
-
+const url = process.env.DB_URL;
 const config = {
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   host: process.env.DB_HOST,
   port: process.env.DB_PORT,
   database: process.env.DB_NAME,
-  ssl: {
-    rejectUnauthorized: true,
-    ca: process.env.CERTIFICATE,
-  },
+  // ssl: {
+  //   require: true,
+  //   rejectUnauthorized: false,
+  // },
 };
 
 const db = new Pool(config);
@@ -40,7 +19,7 @@ async function queryDatabase() {
   const client = await db.connect();
   try {
     const res = await client.query("SELECT VERSION()");
-    console.log("Connected to:", res.rows[0].version);
+    console.log("Connected to:", res.rows[0].version.split(' ').slice(0, 2).join(' '));
   } catch (err) {
     console.error('Query error: ', err);
   } finally {
