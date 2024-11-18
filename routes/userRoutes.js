72dd7b45659f4d499df234ddc3a445968e5b6121
stevesdiary <stephenoyeyemi@gmail.com>
2024-11-18 21:23:52 +0100

    Update userRoutes.js

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -2,9 +2,12 @@ const express = require('express');
 const router = express.Router();
 const { validateUser } = require('../middlewares/validate');
 const {userController} = require('../controllers/userController');
-const  validate  = require("../middlewares/validate");
+const { validate } = require("../middlewares/validate");
+const { createUserSchema, idSchema } = require('../validator/validator');
 
 
-router.post('/create', userController.createUser);
-
+router.post('/create', validate(createUserSchema, 'body'), userController.createUser);
+router.get ('/allusers', userController.getAll);
+router.get('/getone/:id', userController.getOne);
+router.delete('/deleteuser/:id', userController.deleteOne);
 module.exports = router;
