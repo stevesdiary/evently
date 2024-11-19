cc22a8bbf7ef5e82a3091595939a9c1ecf1090b7
stevesdiary <stephenoyeyemi@gmail.com>
2024-11-19 23:55:29 +0100

    Install dependencies
    Update dependencies

--- services/userService.js
+++ services/userService.js
@@ -1,21 +1,59 @@
-const validator = require("../middlewares/validator");
+const { validateUser } = require("../middlewares/validate");
 const { User } = require("../models");
 const userController = require('../controllers/userController');
 const userService = {
 	createUser: async(payload) => {
 		try {
-			const { error } = validator.User.validate(payload);
-			if (error) {
-					throw new Error(error.details[0].message);
-			}
 			const createUser = await User.create(payload);
-			console.log("RESULT", createUser);
 			return createUser;
 		} catch (error) {
-			return error;
+			console.log("Service error", error);
+			throw error;
 		}
 
-	}
+	},
+
+	getAll: async(req, res) => {
+		try {
+			const users = await User.findAll({
+				attributes: {
+					exclude: ["createdAt", "updatedAt", "password"]
+				}
+			});
+			return users;
+		} catch (error) {
+			console.error("Error fetching users:", error);
+				throw error;
+		}
+	},
+
+	getOne: async(payload) => {
+		try {
+			const id = payload;
+			const user = await User.findOne({
+				where: { id: payload },
+				attributes: {
+					exclude: ["createdAt", "updatedAt", "password"]
+				}
+			});
+			return user;
+		} catch (error) {
+			throw error;
+		}
+	},
+
+	deleteOne: async(payload) => {
+		try {
+			const id = payload;
+			const user = await User.destroy({
+				where: { id: payload }
+			});
+			return user;
+		} catch (error) {
+			throw error;
+		}
+	},
+
 }
 
 module.exports = { userService };
