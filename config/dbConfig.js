96a967287d76d8326249f429a60174fd81c3d085
stevesdiary <stephenoyeyemi@gmail.com>
2024-11-29 20:49:55 +0100

    Update dbConfig.js

--- config/dbConfig.js
+++ config/dbConfig.js
@@ -1,39 +1,39 @@
-require('dotenv').config();
-const { Pool } = require('pg');
-const url = process.env.DB_URL;
-const config = {
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
-  database: process.env.DB_NAME,
+// require('dotenv').config();
+// const { Pool } = require('pg');
+// const url = process.env.DB_URL;
+// const config = {
+//   user: process.env.DB_USER,
+//   password: process.env.DB_PASSWORD,
+//   host: process.env.DB_HOST,
+//   port: process.env.DB_PORT,
+//   database: process.env.DB_NAME,
   // ssl: {
   //   require: true,
   //   rejectUnauthorized: false,
   // },
-};
+// };
 
-const db = new Pool(config);
+// const db = new Pool(config);
 
-async function queryDatabase() {
-  const client = await db.connect();
-  try {
-    const res = await client.query("SELECT VERSION()");
-    console.log("Connected to:", res.rows[0].version.split(' ').slice(0, 2).join(' '));
-  } catch (err) {
-    console.error('Query error: ', err);
-  } finally {
-    client.release();
-  }
-}
+// async function queryDatabase() {
+//   const client = await db.connect();
+//   try {
+//     const res = await client.query("SELECT VERSION()");
+//     console.log("Connected to:", res.rows[0].version.split(' ').slice(0, 2).join(' '));
+//   } catch (err) {
+//     console.error('Query error: ', err);
+//   } finally {
+//     client.release();
+//   }
+// }
 
-queryDatabase();
+// queryDatabase();
 
-process.on('SIGTERM', () => {
-  pool.end(() => {
-    console.log('Pool has ended');
-    process.exit(0);
-  });
-});
+// process.on('SIGTERM', () => {
+//   pool.end(() => {
+//     console.log('Pool has ended');
+//     process.exit(0);
+//   });
+// });
 
-module.exports = { db };
+// module.exports = { db };
