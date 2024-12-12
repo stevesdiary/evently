65120d310eef6ec69336b4cbc6f9314723c4dca6
stevesdiary <stephenoyeyemi@gmail.com>
2024-12-12 11:59:29 +0100

    Update userController.js

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,49 +1,131 @@
-const { Event, User } = require('../models');
+const { Event, User, Verification } = require('../models');
 const { Op } = require('sequelize');
+const crypto = require("crypto");
 const { userService } = require('../services/userService');
-
-
-// const userController = {
-// 	createUser: async (req, res, next) => {
-// 		try {
-// 			const { first_name, last_name, email, password, phone_number, type } = req.body;
-// 			const createUser = await User.create(payload);
-// 			console.log("RESULT", createUser);
-// 			if (!createUser) {
-// 				return res.status(403).send({message: 'user not created'})
-// 			}
-// 			return createUser;
-// 		} catch (error) {
-// 			return res.json(error);
-// 		}
-// 	}
-// };
-
-// module.exports = { userController };
-
+const sendVerificationCode = require("../services/emailService");
+const bcrypt = require("bcrypt");
+const salt = 10;
 const userController = {
 	createUser: async (req, res, next) => {
 		try {
-			
-			const { first_name, last_name, email, password, phone_number, type } = req.body;
-
-			const payload = { first_name, last_name, email, password, phone_number, type };
+			const { first_name, last_name, email, password, confirm_password, phone_number, type } = req.body;
+			if (password !== confirm_password) {
+				return res.status(400).json({ message: "Passwords do not match" });
+			}
+			const hashed = await bcrypt.hash(password, salt);
+			const payload = { first_name, last_name, email, password: hashed, phone_number, type };
 			const createUser = await userService.createUser(payload);
 
 			if (!createUser) {
-				return res.status(403).send({ message: 'User not created' });
+				return res.status(createUser.status).json({ message: (createUser.message) });
 			}
-			
-			return res.status(201).json({
-				message: 'User created successfully',
-				data: createUser
-			});
+			const verificationCode = crypto
+				.randomInt(100000, 999999)
+				.toString()
+				.padStart(6, 0);
+			if (createUser) {
+				await Verification.create({
+					email: email,
+					code: verificationCode,
+					expires_at: new Date(Date.now() + 15 * 60 * 1000),
+				})
+				await sendVerificationCode(email, verificationCode);
+				
+				return res.status(createUser.status).json({
+					message: `You have been registered, check your email ${email} for verification code. ${createUser.message}`,
+					data: createUser.data,
+				});
+			}
 		} catch (error) {
-			// Return error response with status code 500
+			console.log(error);
 			return res.status(500).json({
 				message: 'An error occurred while creating the user',
 				error: error
 			});
+		}
+	},
+
+	verifyEmail: async (req, res) => {
+		try {
+			const { email, code } = req.queery;
+			const verificationRecord = await Verification.findOne({
+				where: { email, code, expires_at: { [Op.gt]: new Date() } },
+			});
+			if (!verificationRecord) {
+				return res.status(400).json({ message: "Verification code is invalid or has expired" });
+			}
+			await Verification.update(
+				{ verified: true },
+				{ where: { email: email } }
+			)
+			return res.status(200).json({ message: 'Email verified suxxessfully'})
+		} catch (error) {
+			console.log("Error", error);
+			res.status(500).json({ message: `Server error`, error });
+		}
+	},
+
+	updateUser: async (req, res,) => {
+		try {
+			const user = await userService.findOne({ email: req.params.email });
+			if (!user) {
+				return res.status(404).json({ message: `User with email: ${req.params.email} not found` });
+			}
+			const { first_name, last_name, phone_number } = req.body;
+			if (first_name !== last_name) {
+				
+			}
+		} catch (error) {
+			
+		}
+	},
+
+	getAll: async (req, res) => {
+		try {
+			const users = await userService.getAll();
+			return res.status(users.status).json({
+				message: (users.message),
+				data: (users.data)
+			});
+		} catch (error) {
+			console.error("Error fetching users:", error);
+				return res.status(500).json({
+					message: 'Internal Server Error',
+					error: error.message
+				});
+		}
+	},
+
+	getOne: async (req, res) => {
+		try {
+			const result = await userService.getOne(req.params.id);
+			if (!result) {
+				return res.status(result.status).json({
+					message: result.message,
+				});
+			}
+			return res.status(result.status).json({
+				message: (result.message),
+				...(result.data && { data: result.data})
+			});
+		} catch (error) {
+			return res.status(500).json({
+				message : "Error showed up",
+				error : error.message || error
+			})
+		}
+	},
+
+	deleteOne: async (req, res) => {
+		try {
+			const result = await userService.deleteOne(req.params.id);
+			return res.status(result.status).send({ message: (result.message )});
+		} catch (error) {
+			console.error("Error", error)
+			return res.status(500).json({
+				message : "Error occured",
+				error : error
+			})
 		}
 	}
 };
