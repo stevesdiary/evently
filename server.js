0ebab83c72ae43fa07c0cf0c4b8cbd8fb7d1e3f4
stevesdiary <stephenoyeyemi@gmail.com>
2024-12-12 11:58:47 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,22 @@
 require('dotenv').config();
 const app = require('./app');
+const express = require('express');
+const bodyParser = require('body-parser');
+const webPush = require("web-push");
+const path = require('path');
+const client = require("./config/neonDb");
+const PORT = process.env.LOCAL_PORT || 5000;
+app.use(express.static(path.join(__dirname, 'client')));
 
-const PORT = process.env.PORT || 5000;
+// CORS middleware
+app.use((req, res, next) => {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, PATCH, DELETE");
+    res.setHeader("Access-Control-Allow-Headers", "*");
+    res.setHeader("Access-Control-Allow-Credentials", true);
+    next();
+});
+
 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
